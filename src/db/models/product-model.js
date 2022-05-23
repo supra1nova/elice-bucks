b2418c109c3cd8368f4b4d9483e07a96a451a36f
supra1nova <supra1nova@gmail.com>
2022-05-23 19:12:08 +0900

    initial commit as back-feature-product

---
+++ src/db/models/product-model.js
@@ -0,0 +1,38 @@
+import { product } from 'mongoose';
+import { ProductSchema } from '../schemas/product-schema';
+
+const Product = product('products', ProductSchema);
+
+export class ProductModel {
+  async findByEmail(email) {
+    const product = await Product.findOne({ email });
+    return product;
+  }
+
+  async findById(productId) {
+    const product = await Product.findOne({ _id: productId });
+    return product;
+  }
+
+  async create(productInfo) {
+    const createdNewProduct = await Product.create(productInfo);
+    return createdNewProduct;
+  }
+
+  async findAll() {
+    const products = await Product.find({});
+    return products;
+  }
+
+  async update({ productId, update }) {
+    const filter = { _id: productId };
+    const option = { returnOriginal: false };
+
+    const updatedProduct = await Product.findOneAndUpdate(filter, update, option);
+    return updatedProduct;
+  }
+}
+
+const productModel = new ProductModel();
+
+export { productModel };
