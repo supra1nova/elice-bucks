9cbd7a1904dd6530cfcfa604464c3ddfd4ee033b
supra1nova <supra1nova@gmail.com>
2022-05-23 19:49:35 +0900

    제품 모델 초안

--- src/db/models/product-model.js
+++ src/db/models/product-model.js
@@ -4,26 +4,31 @@ import { ProductSchema } from '../schemas/product-schema';
 const Product = product('products', ProductSchema);
 
 export class ProductModel {
-  async findByEmail(email) {
-    const product = await Product.findOne({ email });
-    return product;
-  }
-
+  // 제품id에 따른 검색
   async findById(productId) {
     const product = await Product.findOne({ _id: productId });
     return product;
   }
-
-  async create(productInfo) {
-    const createdNewProduct = await Product.create(productInfo);
-    return createdNewProduct;
+  
+  // 제품명에 따른 검색
+  async findByTitle(title) {
+    const product = await Product.find({ title });
+    return product;
   }
-
+  
+  // 제품 전체 검색
   async findAll() {
     const products = await Product.find({});
     return products;
   }
 
+  // 제품 생성
+  async create(productInfo) {
+    const createdNewProduct = await Product.create(productInfo);
+    return createdNewProduct;
+  }
+
+  // 제품 관련 업데이트?? 어떤걸 업데이트할지에 따라 수정/추가 필요
   async update({ productId, update }) {
     const filter = { _id: productId };
     const option = { returnOriginal: false };
