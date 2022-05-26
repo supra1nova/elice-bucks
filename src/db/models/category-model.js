2411b2beb0586a68ca668579364d0abc1d3512a7
supra1nova <supra1nova@gmail.com>
2022-05-27 00:43:45 +0900

    [update] category 관련 수정

--- src/db/models/category-model.js
+++ src/db/models/category-model.js
@@ -4,27 +4,33 @@ import { CategorySchema } from '../schemas/category-schema';
 const Category = model('categories', CategorySchema);
 
 export class CategoryModel {
-  // 카테고리 목록 조회
+  // 1. 카테고리 목록 조회
   async findAll() {
     const categories = await Category.find({});
     return categories;
   }
 
-  // 카테고리 이름 기준 조회 - 관리자, 유저
+  // 2. 카테고리 id 기준 조회
+  async findById(categoryId) {
+    const category = await Category.findOne({ _id: categoryId });
+    return category;
+  }
+
+  // 3. 카테고리 이름 기준 조회
   async findByCategory(categoryName) {
     const category = await Category.findOne({ name: categoryName });
     return category;
   }
 
-  // 카테고리 생성
+  // 4. 카테고리 생성
   async create(categoryInfo) {
     const createdNewCategory = await Category.create(categoryInfo);
     return createdNewCategory;
   }
 
-  // 카테고리 수정
-  async update({ categoryName, update }) {
-    const filter = { name: categoryName };
+  // 5. 카테고리 수정
+  async update({ categoryId, update }) {
+    const filter = { _id: categoryId };
     const option = { returnOriginal: false };
     const updatedCategory = await Category.findOneAndUpdate(
       filter,
@@ -34,9 +40,9 @@ export class CategoryModel {
     return updatedCategory;
   }
 
-  // 카테고리 삭제
-  async del( categoryName ) {
-    await Category.deleteOne({ name: categoryName });
+  // 6. 카테고리 삭제
+  async del(categoryId) {
+    await Category.deleteOne({ _id: categoryId });
     return 'Successfully deleted';
   }
 }
