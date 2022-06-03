a04aa2fdce09078377f0fe2607658a337598d548
[SW2/서울/2반]김한얼 <cjswogksdjf@gmail.com>
2022-06-03 12:12:35 +0900

    배송정보 alert수정

--- src/views/myOrder/myOrder.js
+++ src/views/myOrder/myOrder.js
@@ -2,7 +2,7 @@ import * as Api from '/api.js';
 import headerNavbar from '../components/headerNavbar.js';
 import insertCategoryList from '../components/navCategoryList.js';
 import logincheck from '../order/logincheck.js';
-
+// import Swal from 'sweetalert2';
 // navbar 로그인 상태에 따른 로그인 메뉴 삽입
 const headerNavbar1 = document.querySelector('#headerNavbar');
 const ordersContainer = document.querySelector('#ordersContainer');
@@ -71,6 +71,16 @@ async function setUserOrders(orders) {
     let product = '';
     let orderId = orders[i][0].orderId._id;
     let deletedAt = orders[i][0].orderId.deletedAt.match(/\d\d\d\d/g, '');
+    // let paid = orders[i][0].orderId.paid.match(/\d\d\d\d/g, ''); 결제 기능 구현 못함
+    let delivered = orders[i][0].orderId.delivered.match(/\d\d\d\d/g, '');
+    let status = '준비중';
+    if (Number(deletedAt) === 1970) {
+      if (Number(delivered) !== 1970) {
+        status = '배송중';
+      }
+    } else {
+      status = '취소완료';
+    }
     let receiverName = orders[i][0].orderId.receiverName;
     let receiverPhoneNumber = orders[i][0].orderId.receiverPhoneNumber.replace(
       /^(\d{2,3})(\d{3,4})(\d{4})$/,
@@ -95,9 +105,7 @@ async function setUserOrders(orders) {
       <div class="orderprice">${price} 원</div>
     </div>
     <div class="column is-1 div-center orderStatus">
-      <div class="Status">${
-        Number(deletedAt) === 1970 ? '준비중' : '취소완료'
-      }</div>
+      <div class="Status">${status}</div>
     </div>
     <div class="column is-1 div-center">
       <button class="button deleteButton" id="${orderId}">
@@ -106,40 +114,20 @@ async function setUserOrders(orders) {
     </div>
     <ul class="main-menu column is-1 orderStatus">
         <li class="item">
-          <div class="item__name">배송정보</div>
-          <div>
-          <div class="item__contents">
-            <div class="contents__menu container orders-container box">
-              <ul class="inner">
-                <li class="column is-3">
-                  <h4>수령인</h4>
-                  <ul >
-                    <li >${receiverName}</li>
-                  </ul>
-                </li>
-                <li class="column is-3"> 
-                  <h4>휴대폰 번호</h4>
-                  <ul>
-                    <li>${receiverPhoneNumber}</li>
-                  </ul>
-                </li>
-                <li class="column is-4">
-                  <h4>주소</h4>
-                  <ul>
-                    <li>우편번호 : ${postalCode}</li>
-                    <li>${address1}</li>
-                    <li>${address2}</li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-          </div>
+          <div class="item__name delivery">배송정보</div>
         </li>
       </div>
   </div>
   `;
     ordersContainer.insertAdjacentHTML('beforeend', order);
+    deliveryBtn(
+      i,
+      receiverName,
+      receiverPhoneNumber,
+      postalCode,
+      address1,
+      address2
+    );
   }
   deleteBntEvnet();
 }
@@ -148,7 +136,7 @@ async function deleteBntEvnet() {
   let deleteBtn = document.getElementsByClassName(`deleteButton`);
   let Status = document.getElementsByClassName(`Status`);
   for (let k = 0; k < deleteBtn.length; k++) {
-    Status[k].innerHTML === '취소완료' ? (deleteBtn[k].disabled = true) : true;
+    Status[k].innerHTML === '취소완료' ? (deleteBtn[k].disabled = true) : true; // 취소 완료시 deleteBtn 비활성화
     deleteBtn[k].addEventListener('click', async () => {
       try {
         await Api.patch(`/api/order/cancel/${deleteBtn[k].id}`);
@@ -159,4 +147,24 @@ async function deleteBntEvnet() {
       }
     });
   }
+}
+
+function deliveryBtn(
+  i,
+  receiverName,
+  receiverPhoneNumber,
+  postalCode,
+  address1,
+  address2
+) {
+  let delivery = document.getElementsByClassName(`delivery`)[i];
+  delivery.addEventListener('click', () => {
+    Swal.fire(
+      `수령인 : ${receiverName}
+       휴대폰 :${receiverPhoneNumber}
+       우편번호 :${postalCode}
+       주소 :${address1}
+       ${address2}`
+    );
+  });
 }
